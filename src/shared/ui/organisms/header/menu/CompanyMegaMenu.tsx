533c6bef13fba91dbf596411ffcb03a7104de977
aledash3 <cruzd2249@gmail.com>
2026-03-07 14:44:33 -0500

    Cambio de id general a id unico en los MegaMenus

--- src/shared/ui/organisms/header/menu/CompanyMegaMenu.tsx
+++ src/shared/ui/organisms/header/menu/CompanyMegaMenu.tsx
@@ -116,14 +116,14 @@ const CompanyMegaMenu = ({isMobile = false}: CompanyMegaMenuProps) => {
         >
             <button
                 className="text-sm font-medium hover:text-gray-200 transition"
-                aria-controls="services-menu"
+                aria-controls="company-menu"
                 aria-expanded={open}
             >
                 Empresa
             </button>
 
             <div
-                id="services-menu"
+                id="company-menu"
                 role="menu"
                 className={`absolute left-0 top-full mt-4 z-50 w-[800px] bg-white dark:bg-gray-900 rounded-lg shadow-2xl border border-gray-200 dark:border-gray-800 p-6 grid grid-cols-2 gap-6 text-sm transition-all duration-200 ${
                     open ? "opacity-100 visible" : "opacity-0 invisible"
