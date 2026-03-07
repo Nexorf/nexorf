84b4d7cbff14991c83e8fdd4023958e59a1ec7f5
Boxvi <hermosokawai@gmail.com>
2026-03-06 22:27:57 -0500

    feature register part one

---
+++ src/features/landing/pages/register/components/Register.tsx
@@ -0,0 +1,16 @@
+export default function Register() {
+    return (
+        <section className="bg-white dark:bg-black py-20 px-4 min-h-screen flex items-center">
+            <div className="container mx-auto">
+                <div className="max-w-md mx-auto bg-gray-100 dark:bg-gray-800 p-8 rounded-lg shadow-md">
+                    <h2 className="text-3xl font-bold text-center mb-2">
+                        Registrarse
+                    </h2>
+                    <p className="text-center text-gray-600 dark:text-gray-400 mb-6">
+                        Crea tu cuenta
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
